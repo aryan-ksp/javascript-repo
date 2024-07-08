55cd3b28757173a934349d559fcede362ba55d06
Aryan Kashyap <aryn.ksp@gmail.com>
2024-07-08 07:01:55 +0000

    update

---
+++ basics/variables_1.js
@@ -0,0 +1,13 @@
+const accountId = 123 // fixed value using const, not changable
+let accountEmail = "aryn.ksp@gmail.com"
+var accountPassword = "12345"
+
+// var isnt used now as it caused some scope issue, read on google for more details 
+accountcity = "nahan"
+
+//lets try changing it 
+//nodeaccountId = 1234
+let accountstate;
+// semicolon used or not used means same in js
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity, accountstate])
