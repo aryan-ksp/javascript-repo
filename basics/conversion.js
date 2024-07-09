fa825ce2b1b622a1b213cd6c23130c1be4704834
Aryan Kashyap <aryn.ksp@gmail.com>
2024-07-09 07:40:28 +0000

    conversion file added

---
+++ basics/conversion.js
@@ -0,0 +1,23 @@
+//let score = 33
+let score ="33"
+console.log(typeof(score))
+let intoNumber = Number(score)
+console.log(typeof intoNumber)
+
+// try again
+
+let value = "33abc"
+console.log(typeof value)
+let ValueIntoNumber = Number(value)
+console.log(typeof ValueIntoNumber) // prints number, but abc is there as well, which cant be converted into number from above atep
+console.log(ValueIntoNumber) // returns Nan
+
+// lets try again
+let tryy = null
+let tryyy = undefined
+console.log(tryy)
+console.log(tryyy)
+console.log(typeof tryy)
+console.log(typeof tryyy)
+
+
