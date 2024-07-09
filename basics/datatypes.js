cc835a7ab45f4e9ec014dd716ff937c128232938
Aryan Kashyap <aryn.ksp@gmail.com>
2024-07-09 07:18:01 +0000

    new file added

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all JS code as newer version 
+// alert(3+3) , we are using node.js not web browser
+console.log(3+3)
+console.log("hello aryan")
+// js doesnt have that much of datatypes
+let name = "aryan" // string
+let age =21 // int
+let isloggedIn = true // bool
+
+console.table([name,age,  isloggedIn])
+
+// some datatypes
+/* primitve datatypes..........
+number => 2 to power 53
+bigInt == for a big integer value 
+string
+boolean
+null = standalone value 
+undefined = value not assigned
+symbol = to define or find uniqueness ( mostly in react)
+
+objects..........
+*/
+console.log(typeof(isloggedIn))
+console.log(typeof (null)) // gives object, INTERVIEW QUESTION WHY
+console.log(typeof (undefined)) // gives undefined, INTERVIEW QUESTION WHY
