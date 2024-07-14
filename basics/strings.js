da8116c36d9a508a829c34fdc39f85e2b0064499
Aryan Kashyap <aryn.ksp@gmail.com>
2024-07-14 11:03:29 +0000

    strings update

--- basics/strings.js
+++ basics/strings.js
@@ -8,4 +8,14 @@ console.log(name + repoCount + " hahaha");
 
 //another syntax to write 
 
-console.log(' Hello, My name is ${name} and my RepoCount is ${repoCount}');
+console.log(`Hello, My name is ${name} and my RepoCount is ${repoCount}`); // use this ` not ' this '
+
+const gameName = new String('powerRangerS')
+console.log(gameName);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase('S'));
+console.log(gameName.lastIndexOf('r'));
+console.log(gameName.charAt('R'));
+
+console.log(gameName.slice(-11,0));
