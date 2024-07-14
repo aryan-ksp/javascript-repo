c5bf71abbbd42dbb9bd429d52d4ffa105fcb48a7
Aryan Kashyap <aryn.ksp@gmail.com>
2024-07-14 06:48:10 +0000

    updation

---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+// strings can be written as double quote and single quote both
+
+const name = "aryan"
+const repoCount = 50
+console.log(name + repoCount);
+console.log(name +  repoCount + "hahaha")
+console.log(name + repoCount + " hahaha");
+
+//another syntax to write 
+
+console.log(' Hello, My name is ${name} and my RepoCount is ${repoCount}');
